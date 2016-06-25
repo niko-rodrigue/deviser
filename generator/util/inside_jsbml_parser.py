3fb95db378869f069ea61de0668ff6582d9d6c43
hovo1990 <hovakim_grabski@yahoo.com>
2016-06-26 01:17:38 +0400

    extracts jsbml class methods arguments

--- generator/util/inside_jsbml_parser.py
+++ generator/util/inside_jsbml_parser.py
@@ -14,6 +14,64 @@ def clean_line(data):
             temp.append(i)
     return temp
 
+def extract_data(temp_data):
+    print(temp_data)
+    # function_name_step1  = temp_data[-1].split(');')
+    # print(function_name_step1)
+
+    function_name = ''
+    access_member = None
+    is_abstract = False
+    return_type = []
+    arguments = []
+    of_type = []
+
+    for i in range(len(temp_data)):
+
+        # Function Arguments extracter
+        if '(' in temp_data[i]:
+            print('i is ',i)
+            function_name_step1  = temp_data[i].split('(')
+            print('function_name_step1 ',function_name_step1)
+            function_name = function_name_step1[0]
+            function_index = i
+            if function_name_step1[-1] != ');':
+                if ');' in function_name_step1[-1]:
+                    arg = function_name_step1[-1].split(');')[0]
+                    arguments.append(arg)
+                else:
+                    arg = function_name_step1[-1].split(',')[0]
+                    arguments.append(arg)
+                    for y in range(function_index, len(temp_data)):
+                        #print('y ',temp_data[y])
+                        if ',' in temp_data[y]:
+                            arg = function_name_step1[-1].split(',')[0]
+                            arguments.append(arg)
+                        elif ');' in function_name_step1[-1]:
+                            arg = function_name_step1[-1].split(');')[0]
+                            arguments.append(arg)
+            elif function_name_step1[-1] == ');':
+                break
+
+            # else:
+            #     arg = function_name_step1[-1].split(',')[0]
+            #     arguments.append(arg)
+            #     for y in range(len(temp_data[function_index+1:])):
+            #         print('y is ',temp_data[y])
+            print(function_name,'--', function_index) # THis works
+            # function_name_step2  = function_name_step1[-1].split(');')
+            # print(function_name_step2)
+        elif '<' in temp_data[i]:
+            pass
+
+    print('function_name ',function_name)
+    print('arguments ',arguments)
+    print('-----------')
+    # function_name = function_name_step2[0] 
+    # print('function_name ',function_name)
+    # print('------------------')           
+
+
 def parse_output(output):
     output_data = {}
     for line in output:
@@ -22,17 +80,17 @@ def parse_output(output):
         # print(data_stage1)       
         data_stage2 = data_stage1[0].split(' ')
         temp_data = clean_line(data_stage2)
-        print(temp_data)
+        # print(line)
+        # print(temp_data)
+    
+        extract_data(temp_data)
         
-
-        function_name_step1  = temp_data[-1].split(');')
-        function_name_step2  = function_name_step1[0].split('(')
-        function_name = function_name_step2[0] 
-        print('function_name ',function_name)
-        if '{' in temp_data:
-            output_data.update({'classInfo':temp_data[:-1]})
-        elif function_name in temp_data:
-            output_data.update({'methods':temp_data})
+        # if '{' in temp_data:
+        #     output_data.update({'classInfo':temp_data[:-1]})
+        # elif function_name in temp_data:
+        #     output_data.update({'methods':temp_data})
+        # print('->'*20)
+        # print('\n')
     return output_data
 
 def get_class_information(class_name=None):
